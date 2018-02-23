11d11614ba9921fb882bc84d1982f515f735141c
discosilvia <discosilvia@gmail.com>
2018-02-23 17:57:58 +0100

    optimized workflow, minor bug fixes

--- client/templates/person/person_edit.js
+++ client/templates/person/person_edit.js
@@ -1,1 +1,10 @@
 Meteor.subscribe('personsallowed');
+
+AutoForm.addHooks('updatePersonsForm', {
+	onSuccess: function (doc) {
+		//console.log("running after hook for CURSIST insert!");
+		//return Session.set('successfulCursist', true);
+		//Router.go('cursistEdit', {_id: this.docId});
+		Router.go('/');
+	}
+})
